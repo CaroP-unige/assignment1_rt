98bf294dd297ff05de5ea041dffd883c1a1fb261
CaroP-unige <carolina.polese@yahoo.it>
2024-11-27 11:01:51 +0000

    Update

--- src/node2.cpp
+++ src/node2.cpp
@@ -1,6 +1,3 @@
-// Il codice implementa il nodo node2, basato sul monitoraggo e controllo della sicurezza per le tartarughe del workspace //
-// Il programma utilizza Subscriber per ottenere informazioni sulla posizione in tempo reale delle tartarughe e Publisher per fermarle in caso di violazioni di confini o collisioni. //
-
 #include <std_srvs/Empty.h>
 #include <iostream>
 #include <string>
@@ -10,12 +7,9 @@
 #include "geometry_msgs/Twist.h" 
 
 int threshold = 2;
-const float ws_min = 1.0;
-const float ws_max = 10.0;
-const int loop_rate = 10; // Hz
 
-ros::Subscriber sub_pose_turtle_1; // si iscrive al topic turtle1/pose per ricevere la posizione della prima tartaruga.
-ros::Subscriber sub_pose_turtle_2; // si iscrive al topic turtle2/pose per ricevere la posizione della seconda tartaruga.
+ros::Subscriber sub_pose_turtle_1;
+ros::Subscriber sub_pose_turtle_2;
 
 ros::Publisher pub_vel_turtle_1;
 ros::Publisher pub_vel_turtle_2;
@@ -25,33 +19,41 @@ turtlesim::Pose pose_turtle_2;
 
 
 void pose_turtle_Callback_1(const turtlesim::Pose::ConstPtr& msg){
+
 	pose_turtle_1 = *msg;
-	check_boundaries(pose_turtle_1, pub_vel_turtle_1, "turtle_1");
+	
+	geometry_msgs::Twist vel;
+
+	if((pose_turtle_1.x > 10.0) || (pose_turtle_1.x < 1.0) || (pose_turtle_1.y > 10.0) || (pose_turtle_1.y < 1.0)){
+		vel.linear.x = 0;
+		vel.linear.y = 0;
+		pub_vel_turtle_1.publish(vel);
+		std::cout << "Error! turtle_1 has tried to go out the window!\n"; 
+	}
 }
 
 void pose_turtle_Callback_2(const turtlesim::Pose::ConstPtr& msg){
+
 	pose_turtle_2 = *msg;
-	check_boundaries(pose_turtle_2, pub_vel_turtle_2, "turtle_2");
+
+	geometry_msgs::Twist vel;
+	
+	if((pose_turtle_2.x > 10.0) || (pose_turtle_2.x < 1.0) || (pose_turtle_2.y > 10.0) || (pose_turtle_2.y < 1.0)){
+		vel.linear.x = 0;
+		vel.linear.y = 0;
+		pub_vel_turtle_2.publish(vel);
+		std::cout << "Error! turtle_2 has tried to go out the window!\n"; 
+	}
 }
 
-void check_boundaries(const turtlesim::Pose& pose, ros::Publisher& publisher, const std::string& turtle_name) {
-    geometry_msgs::Twist vel;
-    if ((pose.x > ws_max) || (pose.x < ws_min) || (pose.y > ws_max) || (pose.y < ws_min)) {
-        vel.linear.x = 0;
-        vel.linear.y = 0;
-        publisher.publish(vel);
-        std::cout << "Error! " << turtle_name << " has tried to go out the window!\n";
-    }
-
 void controll_th(){
-	// Calcolo distanza euclidea tra le tartarughe
+
+	geometry_msgs::Twist vel;
 	float distance = sqrt(pow(( pose_turtle_1.x - pose_turtle_2.x ),2)+pow(( pose_turtle_1.y - pose_turtle_2.y ),2));
 		
-	std::cout << "Distance: " << distance << "\n"; // CONTROLLARE CHE SIA REALMENTE PUBBLICATA
-	// Controllo per bloccare le tartarughe se risultano essere troppo vicine						
-	
+	std::cout << "Distance: " << distance << "\n";
+							
 	if((distance < threshold)){
-		geometry_msgs::Twist vel;
 		vel.linear.x = 0;
 		vel.linear.y = 0;
 		pub_vel_turtle_1.publish(vel);
@@ -65,15 +67,13 @@ int main (int argc, char **argv)
 	ros::init(argc, argv, "node2"); 
 	ros::NodeHandle nh;
 	
-	// Subscriber per ottenere la posizione delle tartarughe
-	sub_pose_turtle_1 = nh.subscribe("turtle1/pose", 1, pose_turtle_Callback_1); 
-	sub_pose_turtle_2 = nh.subscribe("turtle2/pose", 1, pose_turtle_Callback_2); 
+	sub_pose_turtle_1 = nh.subscribe("turtle1/pose", 1, pose_turtle_Callback_1);
+	sub_pose_turtle_2 = nh.subscribe("turtle2/pose", 1, pose_turtle_Callback_2);
 	
-	// Publisher per fermare le tartarughe in caso di violazioni
 	pub_vel_turtle_1 = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel",1);
 	pub_vel_turtle_2 = nh.advertise<geometry_msgs::Twist>("turtle2/cmd_vel",1);
 	
-	ros::Rate rate(loop_rate); // Frequenza di esecuzione
+	ros::Rate rate(10);
 	
 	while(ros::ok()){
 		controll_th();
