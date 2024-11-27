5fb996e6c43bf8ac29e237a5f44f5bb733742060
CaroP-unige <carolina.polese@yahoo.it>
2024-11-27 13:06:38 +0000

    Update

--- src/node1.cpp
+++ src/node1.cpp
@@ -1,3 +1,6 @@
+// The code implements the node 'node1' that allows the user to interact with the simulator 'turtlesim', specifying the movement commands for two turtles (turtle1 e turtle2) using ROS. //
+
+
 #include <std_srvs/Empty.h>
 #include <iostream>
 #include <string>
@@ -6,7 +9,7 @@
 #include "turtlesim/Pose.h"
 #include "turtlesim/Spawn.h"
 #include "geometry_msgs/Twist.h" 
-#include "geometry_msgs/Pose.h"
+
 
 using namespace std;
 
@@ -16,92 +19,95 @@ ros::Publisher pub_2;
 int command, command_1;
 float command_2;
 
+void set_velocity(int direction, float speed, ros::Publisher& publisher) {
+    geometry_msgs::Twist vel;
+    switch (direction) {
+        case 1: // to the right
+            vel.linear.y = -speed;
+            break;
+        case 2: // to the left
+            vel.linear.y = speed;
+            break;
+        case 3: // forward
+            vel.linear.x = speed;
+            break;
+        case 4: // backward
+            vel.linear.x = -speed;
+            break;
+        default:
+            std::cout << "Error: invalid command!\n";
+            return;
+    }
+    publisher.publish(vel);
+}
 
 int main (int argc, char **argv)
 {
-	// inizializzazione nodo
-	ros::init(argc, argv, "node1"); 
-	ros::NodeHandle nh;
+
+	ros::init(argc, argv, "node1"); // Node 'node1' initialization
+	ros::NodeHandle nh; // Creation of an object of type 'NodeHandle' to manage ROS comunication
 	
-	pub_1 = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel",1); //codice relativo alla velocità di turtle1
-	pub_2 = nh.advertise<geometry_msgs::Twist>("turtle2/cmd_vel",1); //codice relativo alla velocità di turtle2
+	// Code to publish the linear and angular velocity messages of the two turtles (type: geometry_msgs::Twist)
+	// Both Publishers publish on the respective turtles' topics (cmd_vel)
 	
-	ros::ServiceClient client1 =  nh.serviceClient<turtlesim::Spawn>("/spawn"); // spawn turtle2
+	pub_1 = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel",1); // related to turtle1
+	pub_2 = nh.advertise<geometry_msgs::Twist>("turtle2/cmd_vel",1); // related to turtle2
+	
+	// Creation of the second turtle (turtle2) using the 'spawn' service from the 'turtlesim' package 
+	
+	ros::ServiceClient client1 =  nh.serviceClient<turtlesim::Spawn>("/spawn"); 
 	turtlesim::Spawn srv2;
-	
-	srv2.request.x = 2.0; // scelgo la posizione di turtle2
+	// Select the name and position of the new turtle
+	srv2.request.x = 2.0; 
 	srv2.request.y = 1.0;
-	srv2.request.theta = 2.0;
+	srv2.request.theta = 2.0; // Orientation of turtle2
+	srv2.request.name = "turtle2"; 
+	client1.call(srv2); // Call to the service to create turtle2
 	
-	srv2.request.name = "turtle2"; // scelgo il nome di turtle2
-	client1.call(srv2);
+	//if (client1.call(srv2)) {
+    	//std::cout << "Turtle2 creata con successo.\n";
+	//} else {
+    	//std::cerr << "Errore: impossibile creare turtle2 tramite servizio /spawn.\n";
+    //return 1;
+	//}
+
+	// Start of the code for user interaction. It is important to note that the program is implemented on the turtle's reference system
 	
-	while(ros::ok(){
-		std::cout << "Quale tartaruga vuoi comandare? \n";
-		cout << "1 = turtel1 \n";
-		cout << "2 = turtel2 \n";
+	while(ros::ok()){
+		std::cout << "Which turtle do you want to move? \n";
+		cout << "1 = turtle1 \n";
+		cout << "2 = turtle2 \n";
 		cin >> command;
 		if((command != 1) && (command != 2)){
-			std::cout << "Error: hai inserito un valore non corretto\n";
-			exit(1);
+			std::cout << "Error: invalid command!\n";
+			continue; // Return to the loop without exiting
 		}
-		cout << "Come vuoi che si muova la tartaruga?\n";
-		cout << "1 = verso DX \n";
-		cout << "2 = verso SX \n";
-		cout << "3 = avanti \n";
-		cout << "4 = indietro \n";
+		cout << "How do you want the turtle to move?\n";
+		cout << "1 = to the right\n";
+		cout << "2 = to the left \n";
+		cout << "3 = forward \n";
+		cout << "4 = backward \n";
 		cin >> command_1;
-		if((command_1 != 1) && (command_1 != 2) && (command_1 != 3) && (command_1 != 4)){/
-			cout << "Error: hai inserito un valore non corretto\n";
-			exit(2);
-		}	
-		cout << "Con quale velocità lineare vuoi che si muova la tartaruga? \n";
-		cin >> command_2; // INSERIRE CONTROLLO
-		geometry_msgs::Twist vel;
+		if((command_1 != 1) && (command_1 != 2) && (command_1 != 3) && (command_1 != 4)){
+			cout << "Error: invalid command!\n";
+			continue;
+		}
+		cout << "At what speed do you want the turle to move? \n";
+		cin >> command_2;
+		
+		if (command_2 <= 0) {
+			cout << "Error: the speed must be positive!\n";
+			continue; 
+		}
+
 		if(command == 1){
-			switch(command_1){
-				case 1:
-					vel.linear.y = -command_2;
-					break;
-				case 2:
-					vel.linear.y = command_2;
-					break;
-				case 3:
-					vel.linear.x = command_2;
-					break;
-				case 4:
-					vel.linear.x = -command_2;
-					break;
-				default:
-					std::cout << "Error!\n";
-					break;
-			}
-			pub_1.publish(vel);
+			set_velocity(command_1, command_2, pub_1);
+		} else if (command == 2) {
+			set_velocity(command_1, command_2, pub_2);
 		}
 
-		if(command == 2){
-			switch(command_1){
-				case 1:
-					vel.linear.y = -command_2;
-					break;
-				case 2:
-					vel.linear.y = command_2;
-					break;
-				case 3:
-					vel.linear.x = command_2;
-					break;
-				case 4:
-					vel.linear.x = -command_2;
-					break;
-				default:
-					std::cout << "Error!\n";
-					break;
-			}	
-			pub_2.publish(vel);
-		}
-
-	ros::spinOnce();
-	sleep(1); 
+		ros::spinOnce();
+		sleep(1); // Pause of 1 second between the various commands
 	}
 	
 	ros::spin(); 
--- src/node2.cpp
+++ src/node2.cpp
@@ -1,3 +1,7 @@
+// The code implements the node 'node2' based on monitoring and controlling the safety of the turtles in the workspace //
+// The program uses a Subsriber to obtain information about the turles' actual positions and a Publisher to stop them in case of boundary violations or collisions //
+
+
 #include <std_srvs/Empty.h>
 #include <iostream>
 #include <string>
@@ -7,53 +11,50 @@
 #include "geometry_msgs/Twist.h" 
 
 int threshold = 2;
+const float ws_min = 1.0;
+const float ws_max = 10.0;
+const int loop_rate = 10; // Hz
 
-ros::Subscriber sub_pose_turtle_1;
-ros::Subscriber sub_pose_turtle_2;
+ros::Subscriber sub_pose_turtle_1; // It subscribes to the topic 'turtle1/pose' to receive the turtle1's position
+ros::Subscriber sub_pose_turtle_2; // It subscribes to the topic 'turtle2/pose' to receive the turtle2's position
 
-ros::Publisher pub_vel_turtle_1;
+ros::Publisher pub_vel_turtle_1; 
 ros::Publisher pub_vel_turtle_2;
 
 turtlesim::Pose pose_turtle_1;
 turtlesim::Pose pose_turtle_2;
 
+// Function to prevent the turtles from colliding with the window
+void check_boundaries(const turtlesim::Pose& pose, ros::Publisher& publisher, const std::string& turtle_name) {
+    geometry_msgs::Twist vel;
+    if ((pose.x > ws_max) || (pose.x < ws_min) || (pose.y > ws_max) || (pose.y < ws_min)) {
+        vel.linear.x = 0;
+        vel.linear.y = 0;
+        publisher.publish(vel);
+        std::cout << "Error! " << turtle_name << " has tried to go out the window!\n";
+    }
+}
 
 void pose_turtle_Callback_1(const turtlesim::Pose::ConstPtr& msg){
-
 	pose_turtle_1 = *msg;
-	
-	geometry_msgs::Twist vel;
-
-	if((pose_turtle_1.x > 10.0) || (pose_turtle_1.x < 1.0) || (pose_turtle_1.y > 10.0) || (pose_turtle_1.y < 1.0)){
-		vel.linear.x = 0;
-		vel.linear.y = 0;
-		pub_vel_turtle_1.publish(vel);
-		std::cout << "Error! turtle_1 has tried to go out the window!\n"; 
-	}
+	check_boundaries(pose_turtle_1, pub_vel_turtle_1, "turtle_1");
 }
 
 void pose_turtle_Callback_2(const turtlesim::Pose::ConstPtr& msg){
-
 	pose_turtle_2 = *msg;
-
-	geometry_msgs::Twist vel;
-	
-	if((pose_turtle_2.x > 10.0) || (pose_turtle_2.x < 1.0) || (pose_turtle_2.y > 10.0) || (pose_turtle_2.y < 1.0)){
-		vel.linear.x = 0;
-		vel.linear.y = 0;
-		pub_vel_turtle_2.publish(vel);
-		std::cout << "Error! turtle_2 has tried to go out the window!\n"; 
-	}
+	check_boundaries(pose_turtle_2, pub_vel_turtle_2, "turtle_2");
 }
 
+// Function to prevent the turtles from colliding with each other
 void controll_th(){
-
-	geometry_msgs::Twist vel;
+	// Compute the the Euclidean distance between the turtles
 	float distance = sqrt(pow(( pose_turtle_1.x - pose_turtle_2.x ),2)+pow(( pose_turtle_1.y - pose_turtle_2.y ),2));
 		
-	std::cout << "Distance: " << distance << "\n";
-							
+	std::cout << "Distance: " << distance << "\n"; // CONTROLLARE CHE SIA REALMENTE PUBBLICATA
+	
+	// Control to stop the turtle if they are too close to each other						
 	if((distance < threshold)){
+		geometry_msgs::Twist vel;
 		vel.linear.x = 0;
 		vel.linear.y = 0;
 		pub_vel_turtle_1.publish(vel);
@@ -67,13 +68,15 @@ int main (int argc, char **argv)
 	ros::init(argc, argv, "node2"); 
 	ros::NodeHandle nh;
 	
-	sub_pose_turtle_1 = nh.subscribe("turtle1/pose", 1, pose_turtle_Callback_1);
-	sub_pose_turtle_2 = nh.subscribe("turtle2/pose", 1, pose_turtle_Callback_2);
+	// Subscriber to obtain the positions of the turtles
+	sub_pose_turtle_1 = nh.subscribe("turtle1/pose", 1, pose_turtle_Callback_1); 
+	sub_pose_turtle_2 = nh.subscribe("turtle2/pose", 1, pose_turtle_Callback_2); 
 	
+	// Publisher to stop the turtles in case of violations
 	pub_vel_turtle_1 = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel",1);
 	pub_vel_turtle_2 = nh.advertise<geometry_msgs::Twist>("turtle2/cmd_vel",1);
 	
-	ros::Rate rate(10);
+	ros::Rate rate(loop_rate); // Execution frequency
 	
 	while(ros::ok()){
 		controll_th();
